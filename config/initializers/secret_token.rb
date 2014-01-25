b9570875a824d7d82e5fe7c2e6ccec491d3432ba
Tammam Kbeili <tammam.kbeili@gmail.com>
2014-01-25 12:31:23 -0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PostDawg::Application.config.secret_key_base = '448421901883a63e2094c87dc9c3efe41bf53957aa1351f0590a4ff1fe6a34b1b7366f0ae020f5be37a0a72374ae83a07bbe8c9fb08708d36d87e4ad72eef89e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PostDawg::Application.config.session_store :cookie_store, key: '_post_dawg_session'
