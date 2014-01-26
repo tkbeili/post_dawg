8c339afbb474bd3866f4444cb3742e0deadb0270
Tammam Kbeili <tammam.kbeili@gmail.com>
2014-01-25 16:07:53 -0800

    implement edit and delete for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:edit, :show, :destroy, :update]
 
   def index
     @posts = Post.all
@@ -18,10 +19,33 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find params[:id]
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to @post, notice: "Post updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to posts_path, notice: "Post deleted successfully."
+    else
+      redirect_to posts_path, alert: "Problem deleting post."
+    end
+  end
+
+
   private
+
+  def find_post
+    @post = Post.find params[:id]
+  end
 
   def post_params
     params.require(:post).permit([:title, :link, :body, :category_id, :address, :image])
