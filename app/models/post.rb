9badfaa808b6e3e64dc41f1035748b58079f246b
Tammam Kbeili <tammam.kbeili@gmail.com>
2014-01-25 17:29:10 -0800

    show link

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,15 +9,21 @@ class Post < ActiveRecord::Base
 
   validates_attachment_size :image, less_than: 10.megabytes
 
-  geocoded_by :full_street_address  
+  before_save :make_url_proper
+
+  geocoded_by :address  
   after_validation :geocode  
-  
+
   def has_address?
     longitude.present? && latitude.present?
   end
-  
-  def full_street_address
-    address
+
+  private
+
+  def make_url_proper
+    if link.present? && !link.include?("http")
+      self.link = "http://" + link
+    end
   end
 
 end
